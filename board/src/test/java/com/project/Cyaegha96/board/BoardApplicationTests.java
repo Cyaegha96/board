f6aa3e2cb0f28a14455fc740e543ae1dc32e302c
Cyaegha96 <zazae51@gmail.com>
2021-11-20 14:51:13 +0900

    Initial commit

---
+++ board/src/test/java/com/project/Cyaegha96/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Cyaegha96.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
